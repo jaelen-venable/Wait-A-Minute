fe854d2f53e6ba2ef0d29bb481c9ffdef097d915
Jaelen Venable <jav91960@yahoo.com>
2022-12-14 16:35:29 -0500

    user routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes.js";
 
+
+
 const app = express();
-
-
+app.use(express.json());
 app.use("/api/user", router);
 mongoose
     .connect(
