b53467ea21489aea7c89319ecf01124aa2032e63
Jaelen Venable <jav91960@yahoo.com>
2022-12-14 17:25:51 -0500

    user login

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const getAllUser = async (req, res, next) => {
     let users;
@@ -15,7 +16,6 @@ export const getAllUser = async (req, res, next) => {
 
 export const signup = async (req, res, next) => {
     const {name, email, password} = req.body;
-
     let existingUser;
     try {
         existingUser = await User.findOne({email});
@@ -25,10 +25,11 @@ export const signup = async (req, res, next) => {
     if (existingUser) {
         return res.status(400).json({message: "User Already Exists! Login Instead"})
     }
+    const hashedPassword = bcrypt.hashSync(password);
     const user = new User ({
         name,
         email,
-        password
+        password: hashedPassword
     });
 
     try {
@@ -37,4 +38,23 @@ export const signup = async (req, res, next) => {
     return console.log(err);
     }
     return res.status(201).json({user})
+}
+
+export const login = async (req, res, next) => {
+    const { email, password} = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email});
+    } catch (err) {
+    return console.log(err);
+    }
+    if (existingUser) {
+        return res.status(404).json({message: "Couldnt Find User"});
+    }
+
+    const isPasswordCorrect = bcrpyt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect){
+        return res.status(400),json({message: "Incorrect Password"})
+    }
+    return res.status(200).json({message: "Login Successfull"})
 }
