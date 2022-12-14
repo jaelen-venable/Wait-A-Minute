1868205fd7be8120c30430c2e97e988a29f14ef5
Jaelen Venable <jav91960@yahoo.com>
2022-12-14 16:01:08 -0500

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/user-routes.js";
+
 const app = express();
 
+
+app.use("/api/user", router);
 mongoose
     .connect(
         "mongodb+srv://jaeven:donut@cluster0.fnbx45f.mongodb.net/WaitAMinute=true&w=majority"
 )
-.then(() => app.listen(3002))
+.then(() => app.listen(3000))
 .then(() => 
     console.log("Connected TO Database and Listening TO Local Host 3000")
 )
