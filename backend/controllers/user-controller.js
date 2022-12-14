274e92bdbb8e9edff13fe5645ff1c5dc4745544d
Jaelen Venable <jav91960@yahoo.com>
2022-12-14 16:27:59 -0500

    users

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -12,3 +12,29 @@ export const getAllUser = async (req, res, next) => {
     }
     return res.status(200).json({users});
 };
+
+export const signup = async (req, res, next) => {
+    const {name, email, password} = req.body;
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email});
+    } catch (err) {
+        console.log(err);
+    }
+    if (existingUser) {
+        return res.status(400).json({message: "User Already Exists! Login Instead"})
+    }
+    const user = new User ({
+        name,
+        email,
+        password
+    });
+
+    try {
+        user.save()
+    } catch (err) {
+        console.log(err);
+    }
+    return res.status(201).json({user})
+}
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -4,5 +4,6 @@ import { getAllUser } from "../controllers/user-controller.js";
 const router = express.Router();
 
 router.get("/",getAllUser);
+router.post("/signup")
 
 export default router;
