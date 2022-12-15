b39a80fa5f17771827f83b5d7afc30cf17f307c0
Jaelen Venable <jav91960@yahoo.com>
2022-12-15 02:04:24 -0500

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
 import Header from "./components/Header";
+import React from "react";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
 
 function App() {
-  return <Header/>;
+  return <React.Fragment> 
+  <header>
+    <Header/>
+  </header>
+  <main>
+    <Routes>
+      <Route path="/auth" element={<Auth/>}/>
+      <Route path="/blogs" element={<Blogs/>}/>
+      <Route path="/myBlogs" element={<UserBlogs/>}/>
+      <Route path="/myBlogs/:id" element={<BlogDetail/>}/>
+      <Route path="/blogs/add" element={<AddBlog/>}/>
+    </Routes>
+    </main>
+  </React.Fragment>
 }
 
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {AppBar, Button, Toolbar, Typography, Box} from '@mui/material'
+import {AppBar, Button, Toolbar, Typography, Box, Tab, Tabs} from '@mui/material'
+import { useState } from 'react';
 
 const Header = () => {
-    return <AppBar>
+    const [value, setValue] = useState();
+    return <AppBar
+            position="sticky">
         <Toolbar>
             <Typography variant="h4">Wait A Minute</Typography>
+            <Box display="flex">
+                <Tabs textColor="inherit" value={value} onChange={(e, val) =>setValue(val)}>
+            <Tab label="All Blogs"/>
+            <Tab label="My Blogs"/>
+                </Tabs>
+            </Box>
             <Box display="flex" marginLeft="auto">
-                <Button color="warning">Login</Button>
-                <Button color="warning">Signup</Button>
+                <Button variant="warning">Login</Button>
+                <Button variant="warning">Signup</Button>
+                <Button variant="warning">Logout</Button>
             </Box>
         </Toolbar>
     </AppBar>
