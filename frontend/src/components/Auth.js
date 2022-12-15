ef08695f73acca6d2d5de1d2aa21e8c285588798
Jaelen Venable <jav91960@yahoo.com>
2022-12-15 04:11:22 -0500

    submit handle

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -10,11 +10,15 @@ const Auth = () => {
         setInputs((prevState) => ({
             ...prevState,
             [e.target.name] : e.target.value
-        }))
+        }));
+    };
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(inputs);
     }
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Box display="flex" flexDirection={'column'} alignItems='center' boxShadow="10px 10px 20px #ccc" padding={3} margin="auto" marginTop={5} borderRadius={5} maxWidth={400}>
                     <Typography variant="h2" padding={3} textAlign="center">
                     {isSignup ? "Signup" : "Login"}
@@ -22,7 +26,7 @@ const Auth = () => {
                     {isSignup && <TextField name="name" onChange={handleChange} value={inputs.name} placeholder="Name" margin="normal"/>}{""}
                     <TextField name="email" onChange={handleChange} value={inputs.email} type={'email'} placeholder="Email" margin="normal" />
                     <TextField name="password" onChange={handleChange} value={inputs.password} type={'password'} placeholder="Password" margin="normal" />
-                    <Button sx={{borderRadius: 3, marginTop: 3}}variant="contained" color="warning">Submit</Button>
+                    <Button type="submit" sx={{borderRadius: 3, marginTop: 3}}variant="contained" color="warning">Submit</Button>
                     <Button onClick={()=>setSignup(!isSignup)} sx={{borderRadius: 3}}> Change to {isSignup ? "Login":"Signup"}</Button>
                 </Box>
             </form>
