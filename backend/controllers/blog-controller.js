25a38e9c51cdfb3b5255539b7adfd45892074ad2
Jaelen Venable <jav91960@yahoo.com>
2022-12-14 20:47:05 -0500

    delete function

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -29,3 +29,51 @@ try {
 }
 return res.status(200).json({blog})
 };
+
+export const updateBlog = async (req, res, next) => {
+    const { title, description} = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description
+        })
+    } catch (err) {
+        return console.log(err)
+    }
+    if (!blog) {
+        return res.status(500).json({message: "unable to update"})
+    }
+    return res.status(200).json({blog});
+    
+};
+
+export const getById = async (req, res, next) => {
+    const id = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findById(id);
+    } catch (err) {
+        return console.log(err)
+    }
+    if (!blog) {
+        return res.status(404).json({message: " no blog found"})
+    }
+    return res.status(200).json({blog});
+};
+
+export const deleteBlog = async (req, res, next) => {
+    const id = req.params.id;
+
+    let blog;
+    try {
+        blog = await Blog.findByIdAndRemove(id)
+    } catch (err) {
+        console.log(err);
+    }
+    if (!blog) {
+        return res.status(500).json({message: "unable to delete"})
+    }
+    return res.status(200).json({message: "Successfully Delete"})
+}
