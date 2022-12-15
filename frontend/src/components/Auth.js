75cba7d2a988fa1ffc83edacdbd5666c8eac1c91
Jaelen Venable <jav91960@yahoo.com>
2022-12-15 04:06:43 -0500

    auth pages

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
 
 const Auth = () => {
+    const [inputs, setInputs] = useState({
+        name:"", email:"", password:""
+    });
+    const [isSignup, setSignup] = useState(false);
+    const handleChange = (e) =>{
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }))
+    }
     return (
         <div>
             <form>
                 <Box display="flex" flexDirection={'column'} alignItems='center' boxShadow="10px 10px 20px #ccc" padding={3} margin="auto" marginTop={5} borderRadius={5} maxWidth={400}>
-                    <Typography>Login</Typography>
-                    <TextField />
-                    <TextField />
-                    <TextField />
-                    <Button>Submit</Button>
-                    <Button> Change to Signup</Button>
+                    <Typography variant="h2" padding={3} textAlign="center">
+                    {isSignup ? "Signup" : "Login"}
+                    </Typography>
+                    {isSignup && <TextField name="name" onChange={handleChange} value={inputs.name} placeholder="Name" margin="normal"/>}{""}
+                    <TextField name="email" onChange={handleChange} value={inputs.email} type={'email'} placeholder="Email" margin="normal" />
+                    <TextField name="password" onChange={handleChange} value={inputs.password} type={'password'} placeholder="Password" margin="normal" />
+                    <Button sx={{borderRadius: 3, marginTop: 3}}variant="contained" color="warning">Submit</Button>
+                    <Button onClick={()=>setSignup(!isSignup)} sx={{borderRadius: 3}}> Change to {isSignup ? "Login":"Signup"}</Button>
                 </Box>
             </form>
         </div>
