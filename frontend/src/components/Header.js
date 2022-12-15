9293d38f68772973447cf9e6652273259f19fa01
Jaelen Venable <jav91960@yahoo.com>
2022-12-15 01:24:08 -0500

    react router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography} from '@mui/material'
+import {AppBar, Button, Toolbar, Typography, Box} from '@mui/material'
 
 const Header = () => {
     return <AppBar>
         <Toolbar>
             <Typography variant="h4">Wait A Minute</Typography>
+            <Box display="flex" marginLeft="auto">
+                <Button color="warning">Login</Button>
+                <Button color="warning">Signup</Button>
+            </Box>
         </Toolbar>
     </AppBar>
 }
