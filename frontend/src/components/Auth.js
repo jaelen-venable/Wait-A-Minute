ea97857acb7751a9fbb52f9421349b2db577fd39
Jaelen Venable <jav91960@yahoo.com>
2022-12-15 03:09:44 -0500

    signup page

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const Auth = () => {
     return (
-        <div>Login</div>
+        <div>
+            <form>
+                <Box display="flex" flexDirection={'column'} alignItems='center' boxShadow="10px 10px 20px #ccc" padding={3} margin="auto" marginTop={5} borderRadius={5} maxWidth={400}>
+                    <Typography>Login</Typography>
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <Button>Submit</Button>
+                    <Button> Change to Signup</Button>
+                </Box>
+            </form>
+        </div>
     )
-}
+    
+};
 
 export default Auth;
